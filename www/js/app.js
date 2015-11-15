5db72f8ff7ed00142a0a2ab26f7a5b8eb4a4c53c
carlos99 <carlos.cw2106@gmail.com>
2015-11-15 00:21:26 -0600

    Refactorin code, creating the NoteStore Factory in notestore.js file. Implementing the local storage to save data

--- www/js/app.js
+++ www/js/app.js
@@ -1,5 +1,5 @@
 (function (){
-var app = angular.module('starter', ['ionic']);
+var app = angular.module('mynotes', ['ionic', 'mynotes.notestore']);
 
 app.config(function($stateProvider, $urlRouterProvider){
   $stateProvider.state('list', {
@@ -22,35 +22,11 @@ app.config(function($stateProvider, $urlRouterProvider){
   $urlRouterProvider.otherwise('/list');
 });
 
-var notes = [];
-
-  function getNote(noteId){
-    for (var i = 0; i < notes.length; i++){
-      if (notes[i].id === noteId) {
-        return notes[i];
-      }
-    }
-    return undefined;
-  }
-
-  function updateNote(note){
-    for (var i = 0; i < notes.length; i++){
-      if (notes[i].id === note.id) {
-        notes[i] = note;
-        return;
-      }
-    }
-  }
-
-  function createNote(note){
-    notes.push(note);
-  }
-
-app.controller('ListCtrl', function($scope){
-  $scope.notes = notes;
+app.controller('ListCtrl', function($scope, NoteStore){
+  $scope.notes = NoteStore.list();
 });
 
-app.controller('AddCtrl', function($scope, $state){
+app.controller('AddCtrl', function($scope, $state, NoteStore){
 
   $scope.note = {
     id: new Date().getTime().toString(),
@@ -59,17 +35,17 @@ app.controller('AddCtrl', function($scope, $state){
   };
 
   $scope.save = function(){
-    createNote($scope.note);
+    NoteStore.create($scope.note);
     $state.go('list');
   };
 });
 
-app.controller('EditCtrl', function($scope, $state){
+app.controller('EditCtrl', function($scope, $state, NoteStore){
 
-  $scope.note = angular.copy(getNote($state.params.noteId));
+  $scope.note = angular.copy(NoteStore.get($state.params.noteId));
 
   $scope.save = function(){
-    updateNote($scope.note);
+    NoteStore.update($scope.note);
     $state.go('list');
   }
 });
